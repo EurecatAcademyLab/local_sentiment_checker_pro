c64631f1326cedc6e767a8ea434420454a6c60e1
JuanCarlo Castillo <juancarlo.castillo20@gmail.com>
2023-05-22 15:59:11 +0200

    Change version type

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_survey_intelligence';
-$plugin->requires = 2021051700;
+$plugin->requires = 2023051500;
 $plugin->version = 20236110400;
 $plugin->release = "0.1.2";
 $plugin->maturity = MATURITY_ALPHA;
